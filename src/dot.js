06a540565642f7c9928a681613146392006b8d75
Osama Miro <o.miro650@gmail.com>
2018-07-16 23:11:17 -0400

    brktru

--- src/dot.js
+++ src/dot.js
@@ -13,46 +13,55 @@ componentDidMount(){
     this.props.animateFauxDOM(800)
   }
 
-// makeDot = () => {
-//   for(let i = 0; i < this.props.freqs.length; i++){
-//     if(this.props.freqs[i] < 250){
-//     d3.selectAll("circle")
-//     .classed('item--transitioning', true)
-//     .style('fill', 'white')
-//     .attr("r", 30)
-//     .transition().duration(750)
-//     .attr("r", 30)
-//     .style('fill', 'blue')
-//     return <circle cx={40} cy={40} r="20"></circle>
-//   } else {
-//   d3.selectAll("circle")
-//   .classed('item--transitioning', true)
-//   .style('fill', 'white')
-//   .attr("r", 30)
-//   .transition().duration(750)
-//   .attr("r", 20)
-//   .style('fill', 'red')
-//   return <circle cx={60} cy={100} r="20"></circle>
-//     }
-//   }
-// }
-
 makeDot = () => {
-  var p = d3.select("body")
-    .selectAll("p")
-    .data(this.props.freqs)
-    .text(function(d) { return d; });
+  let num1 = 20
+  let num2 = 40
+  let frequencies = this.props.freqs
+  return frequencies.map(user => {
+    num1 += 20
+    num2 += 20
+  return  <circle cx={num1} cy={num2} r="20"></circle>
+})
+  // for(let i = 0; i < this.props.freqs.length; i++){
+  //
+  // }
+  //   if(this.props.freqs[i] < 250){
+  //   d3.selectAll("circle")
+  //   .classed('item--transitioning', true)
+  //   .style('fill', 'white')
+  //   .attr("r", 30)
+  //   .transition().duration(750)
+  //   .attr("r", 30)
+  //   .style('fill', 'blue')
+  //   return <circle cx={40} cy={40} r="20"></circle>
+  // } else {
+  // d3.selectAll("circle")
+  // .classed('item--transitioning', true)
+  // .style('fill', 'white')
+  // .attr("r", 30)
+  // .transition().duration(750)
+  // .attr("r", 20)
+  // .style('fill', 'red')
+  // return <circle cx={60} cy={100} r="20"></circle>
+  //   }
+  // }
 }
+
+// makeDot = () => {
+//   return
+//   let p = d3.select("div")
+//     .selectAll("p")
+//     .data(this.props.freqs)
+//     .text(function(d) { return d; });
+// }
 
 
   render(){
-
+console.log(this.makeDot())
     return(
       <div>
       <svg width="1300" height="720">
-      <p>
       {this.makeDot()}
-      </p>
       </svg>
       </div>
     )
